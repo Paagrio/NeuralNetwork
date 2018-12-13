c23ccf39ca3ff4b2acd3a3867ab2637a7c947be3
Paagrio <denis.malakhoviit@gmail.com>
2018-12-13 13:00:16 +0300

    OneLayer

--- Models/Layer.cs
+++ Models/Layer.cs
@@ -5,197 +5,201 @@ using System.Xml.Serialization;
 
 namespace NeuralNetwork
 {
-    [Serializable]
-    public class Layer
+  [Serializable]
+  public class Layer
+  {
+    public Neuron[] neurons { get; set; }
+    const double LEARNING_RATE = 0.05;
+
+    private Layer()
     {
-        public Neuron[] neurons { get; set; }
-        const double LEARNING_RATE = 0.05;
 
-        private Layer()
-        {
-
-        }
-
-        public Layer(int nCount)
-        {
-            neurons = new Neuron[nCount];
-            for (int i = 0; i < nCount; i++)
-            {
-                neurons[i] = new Neuron();
-            }
-        }
-
-        public void SaveLayer(string path)
-        {
-            BinaryFormatter formatter = new BinaryFormatter();
-            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
-            {
-                formatter.Serialize(fs, this);
-            }
-            Console.WriteLine("serialized");
-        }
-
-        public Layer LoadLayer(string path)
-        {
-            BinaryFormatter formatter = new BinaryFormatter();
-            Layer layer;
-            using (FileStream fs = new FileStream(path, FileMode.Open))
-            {
-                layer = formatter.Deserialize(fs) as Layer;
-            }
-            Console.WriteLine("deserialized");
-            return layer;
-        }
-
-        public void InitNetwork()
-        {
-            Random rnd = new Random();
-            for (int i = 0; i < neurons.Length; i++)
-            {
-                for (int j = 0; j < 28 * 28; j++)
-                {
-                    neurons[i].Input[j] = 0.00;
-                    neurons[i].Weight[j] = rnd.NextDouble();
-                }
-                neurons[i].Output = 0.00;
-            }
-        }
-
-        public void InitNetwork(Layer layer)
-        {
-            for (int i = 0; i < neurons.Length; i++)
-            {
-                for (int j = 0; j < 28 * 28; j++)
-                {
-                    neurons[i].Input[j] = layer.neurons[i].Input[j];
-                    neurons[i].Weight[j] = layer.neurons[i].Weight[j];
-                }
-                neurons[i].Output = layer.neurons[i].Output;
-            }
-        }
-
-
-        public bool TrainLayer(byte[] data, int lbl)
-        {
-            int[] vector = GetOutputVector(lbl);
-            for (int i = 0; i < neurons.Length; i++)
-            {
-                TrainNeuron(neurons[i], data, vector[i]);
-            }
-
-            int predictedNum = GetLayerPrediction(this);
-
-            if (predictedNum != lbl)
-            {
-                return false;
-            }
-            return true;
-        }
-
-        public bool TestLayer(byte[] data, int lbl)
-        {
-            int[] vector = GetOutputVector(lbl);
-            for (int i = 0; i < neurons.Length; i++)
-            {
-                TestNeuron(neurons[i], data, vector[i]);
-            }
-
-            int predictedNum = GetLayerPrediction(this);
-            if (predictedNum != lbl)
-            {
-                return false;
-            }
-            return true;
-        }
-
-        private void TestNeuron(Neuron neuron, byte[] data, int target)
-        {
-            SetNeuronInput(neuron, data);
-            CalcNeuronOutput(neuron);
-        }
-        private void TrainNeuron(Neuron neuron, byte[] data, int target)
-        {
-            SetNeuronInput(neuron, data);
-            CalcNeuronOutput(neuron);
-
-            double error = GetNeuronError(neuron, target);
-            UpdateNeuronWeights(neuron, error);
-        }
-
-        private int[] GetOutputVector(int lbl)
-        {
-            int[] vector = new int[10];
-            for (int i = 0; i < 10; i++)
-            {
-                vector[i] = lbl == i ? 1 : 0;
-            }
-            return vector;
-        }
-
-        private int GetLayerPrediction(Layer layer)
-        {
-            double maxOut = 0;
-            int maxInd = 0;
-
-            for (int i = 0; i < layer.neurons.Length; i++)
-            {
-                if (layer.neurons[i].Output > maxOut)
-                {
-                    maxOut = layer.neurons[i].Output;
-                    maxInd = i;
-                }
-            }
-            return maxInd;
-        }
-
-        private void UpdateNeuronWeights(Neuron neuron, double error)
-        {
-            for (int i = 0; i < neuron.Input.Length; i++)
-            {
-                neuron.Weight[i] += LEARNING_RATE * neuron.Input[i] * error;
-            }
-        }
-
-        private void CalcNeuronOutput(Neuron neuron)
-        {
-            neuron.Output = 0;
-            for (int i = 0; i < neuron.Input.Length; i++)
-            {
-                neuron.Output += neuron.Input[i] * neuron.Weight[i];
-            }
-            neuron.Output = 1.00 / (1.00 + Math.Exp(-neuron.Output));
-            if (neuron.Output < 0) 
-            Console.WriteLine(neuron.Output);
-        }
-
-        private double GetNeuronError(Neuron neuron, int target)
-        {
-            double error = target - neuron.Output;
-            return error;
-        }
-
-        private void SetNeuronInput(Neuron neuron, byte[] data)
-        {
-            for (int i = 0; i < data.Length; i++)
-            {
-                neuron.Input[i] = data[i] > 0 ? 1 : 0;
-            }
-        }
-        public void Write(byte[] pixels)
-        {
-            string s = "";
-            for (int i = 1; i < (28 * 28) + 1; i++)
-            {
-                if (pixels[i - 1] == 0) //белый 
-                    s += "@";
-                else if (pixels[i - 1] == 255) //черный 
-                    s += "O";
-                else
-                    s += "."; //оттенки черного 
-                if (i % 28 == 0)
-                    s += "\n";
-            }
-            Console.WriteLine(s + "\n");
-        }
     }
+
+    public Layer(int nCount)
+    {
+      neurons = new Neuron[nCount];
+      for (int i = 0; i < nCount; i++)
+      {
+        neurons[i] = new Neuron();
+      }
+    }
+
+    public void SaveLayer(string path)
+    {
+      BinaryFormatter formatter = new BinaryFormatter();
+      using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+      {
+        formatter.Serialize(fs, this);
+      }
+      Console.WriteLine("serialized");
+    }
+
+    public Layer LoadLayer(string path)
+    {
+      BinaryFormatter formatter = new BinaryFormatter();
+      Layer layer;
+      using (FileStream fs = new FileStream(path, FileMode.Open))
+      {
+        layer = formatter.Deserialize(fs) as Layer;
+      }
+      Console.WriteLine("deserialized");
+      return layer;
+    }
+
+    public void InitNetwork()
+    {
+      Random rnd = new Random();
+      for (int i = 0; i < neurons.Length; i++)
+      {
+        for (int j = 0; j < 28 * 28; j++)
+        {
+          neurons[i].Input[j] = 0.00;
+          neurons[i].Weight[j] = rnd.NextDouble();
+        }
+        neurons[i].Output = 0.00;
+      }
+    }
+
+    public void InitNetwork(Layer layer)
+    {
+      for (int i = 0; i < neurons.Length; i++)
+      {
+        for (int j = 0; j < 28 * 28; j++)
+        {
+          neurons[i].Input[j] = layer.neurons[i].Input[j];
+          neurons[i].Weight[j] = layer.neurons[i].Weight[j];
+        }
+        neurons[i].Output = layer.neurons[i].Output;
+      }
+    }
+
+
+    public bool TrainLayer(byte[] data, int lbl)
+    {
+      int[] vector = GetOutputVector(lbl);
+      Console.WriteLine("input: " + lbl);
+
+      for (int i = 0; i < neurons.Length; i++)
+      {
+        TrainNeuron(neurons[i], data, vector[i]);
+      }
+
+      int predictedNum = GetLayerPrediction(this);
+      Console.WriteLine("Prediction: " + predictedNum);
+      if (predictedNum != lbl)
+      {
+        return false;
+      }
+      return true;
+    }
+
+    public bool TestLayer(byte[] data, int lbl)
+    {
+      int[] vector = GetOutputVector(lbl);
+      Console.WriteLine("input: " + lbl);
+      for (int i = 0; i < neurons.Length; i++)
+      {
+        TestNeuron(neurons[i], data, vector[i]);
+      }
+
+      int predictedNum = GetLayerPrediction(this);
+      Console.WriteLine("Prediction: " + predictedNum);
+      if (predictedNum != lbl)
+      {
+        return false;
+      }
+      return true;
+    }
+
+    private void TestNeuron(Neuron neuron, byte[] data, int target)
+    {
+      SetNeuronInput(neuron, data);
+      CalcNeuronOutput(neuron);
+    }
+    private void TrainNeuron(Neuron neuron, byte[] data, int target)
+    {
+      SetNeuronInput(neuron, data);
+      CalcNeuronOutput(neuron);
+
+      double error = GetNeuronError(neuron, target);
+      UpdateNeuronWeights(neuron, error);
+    }
+
+    private int[] GetOutputVector(int lbl)
+    {
+      int[] vector = new int[10];
+      for (int i = 0; i < 10; i++)
+      {
+        vector[i] = lbl == i ? 1 : 0;
+      }
+      return vector;
+    }
+
+    private int GetLayerPrediction(Layer layer)
+    {
+      double maxOut = 0;
+      int maxInd = 0;
+
+      for (int i = 0; i < layer.neurons.Length; i++)
+      {
+        if (layer.neurons[i].Output > maxOut)
+        {
+          maxOut = layer.neurons[i].Output;
+          maxInd = i;
+        }
+      }
+      return maxInd;
+    }
+
+    private void UpdateNeuronWeights(Neuron neuron, double error)
+    {
+      for (int i = 0; i < neuron.Input.Length; i++)
+      {
+        neuron.Weight[i] += LEARNING_RATE * neuron.Input[i] * error;
+      }
+    }
+
+    private void CalcNeuronOutput(Neuron neuron)
+    {
+      neuron.Output = 0;
+      for (int i = 0; i < neuron.Input.Length; i++)
+      {
+        neuron.Output += neuron.Input[i] * neuron.Weight[i];
+      }
+      neuron.Output = 1.00 / (1.00 + Math.Exp(-neuron.Output));
+      if (neuron.Output < 0)
+        Console.WriteLine(neuron.Output);
+    }
+
+    private double GetNeuronError(Neuron neuron, int target)
+    {
+      double error = target - neuron.Output;
+      return error;
+    }
+
+    private void SetNeuronInput(Neuron neuron, byte[] data)
+    {
+      for (int i = 0; i < data.Length; i++)
+      {
+        neuron.Input[i] = data[i] > 0 ? 1 : 0;
+      }
+    }
+    public void Write(byte[] pixels)
+    {
+      string s = "";
+      for (int i = 1; i < (28 * 28) + 1; i++)
+      {
+        if (pixels[i - 1] == 0) //белый 
+          s += "@";
+        else if (pixels[i - 1] == 255) //черный 
+          s += "O";
+        else
+          s += "."; //оттенки черного 
+        if (i % 28 == 0)
+          s += "\n";
+      }
+      Console.WriteLine(s + "\n");
+    }
+  }
 }
--- Program.cs
+++ Program.cs
@@ -4,94 +4,97 @@ using NeuralNetwork;
 using System.Drawing;
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    try
     {
-        try
+      Layer nn = new Layer(10);
+      nn.InitNetwork();
+
+      #region Learning
+
+      FileStream ifsLabels = new FileStream("train-labels.idx1-ubyte", FileMode.Open);//поток для чтения лейблов 
+      FileStream ifsImages = new FileStream("train-images.idx3-ubyte", FileMode.Open); // test images 
+
+      BinaryReader brLabels = new BinaryReader(ifsLabels);
+      BinaryReader brImages = new BinaryReader(ifsImages);
+
+      int magic1 = brImages.ReadInt32(); // магическое число 
+      int numImages = brImages.ReadInt32(); //количество изображений 
+      int numRows = brImages.ReadInt32(); //количество строк в изображении 
+      int numCols = brImages.ReadInt32(); //количество столбцов изображения 
+      int magic2 = brLabels.ReadInt32(); //магическое число 
+      int numLabels = brLabels.ReadInt32(); //количество лейблов 
+
+      byte[] pixels = new byte[28 * 28]; //инициализация массива для хранения изображения 28x28 
+      int success = 0;
+
+      for (int di = 0; di < 60000; ++di)
+      {
+        byte lbl = brLabels.ReadByte(); //текущее значение лейбла 
+        for (int i = 0; i < 28 * 28; ++i)
         {
-            Layer nn = new Layer(10);
-            nn.InitNetwork();
-
-            #region Learning
-
-            FileStream ifsLabels = new FileStream("train-labels.idx1-ubyte", FileMode.Open);//поток для чтения лейблов 
-            FileStream ifsImages = new FileStream("train-images.idx3-ubyte", FileMode.Open); // test images 
-
-            BinaryReader brLabels = new BinaryReader(ifsLabels);
-            BinaryReader brImages = new BinaryReader(ifsImages);
-
-            int magic1 = brImages.ReadInt32(); // магическое число 
-            int numImages = brImages.ReadInt32(); //количество изображений 
-            int numRows = brImages.ReadInt32(); //количество строк в изображении 
-            int numCols = brImages.ReadInt32(); //количество столбцов изображения 
-            int magic2 = brLabels.ReadInt32(); //магическое число 
-            int numLabels = brLabels.ReadInt32(); //количество лейблов 
-
-            byte[] pixels = new byte[28 * 28]; //инициализация массива для хранения изображения 28x28 
-            int success = 0;
-
-            for (int di = 0; di < 60000; ++di)
-            {
-                byte lbl = brLabels.ReadByte(); //текущее значение лейбла 
-                for (int i = 0; i < 28 * 28; ++i)
-                {
-                    byte b = brImages.ReadByte();
-                    pixels[i] = b; //считываем байт изображения в массив 
-                }
-                if (nn.TrainLayer(pixels, lbl))
-                {
-                    success++;
-                }
-            }
-            nn.SaveLayer("layer.dat");
-            Console.WriteLine("training success rate: " + success / 60000.00 * 100.00);
-            ifsImages.Close();
-            brImages.Close();
-            ifsLabels.Close();
-            brLabels.Close();
-
-            #endregion
-
-            #region Testing
-
-            ifsLabels = new FileStream("t10k-labels.idx1-ubyte", FileMode.Open);//поток для чтения лейблов 
-            ifsImages = new FileStream("t10k-images.idx3-ubyte", FileMode.Open); // test images 
-            brLabels = new BinaryReader(ifsLabels);
-            brImages = new BinaryReader(ifsImages);
-
-            magic1 = brImages.ReadInt32(); // магическое число 
-            numImages = brImages.ReadInt32(); //количество изображений 
-            numRows = brImages.ReadInt32(); //количество строк в изображении 
-            numCols = brImages.ReadInt32(); //количество столбцов изображения 
-
-            magic2 = brLabels.ReadInt32(); //магическое число 
-            numLabels = brLabels.ReadInt32(); //количество лейблов
-
-            success = 0;
-            nn = nn.LoadLayer("layer.dat");
-            for (int di = 0; di < 10000; ++di)
-            {
-                byte lbl = brLabels.ReadByte(); //текущее значение лейбла 
-                for (int i = 0; i < 28 * 28; ++i)
-                {
-                    byte b = brImages.ReadByte();
-                    pixels[i] = b; //считываем байт изображения в массив 
-                }
-                if (nn.TestLayer(pixels, lbl))
-                {
-                    success++;
-                }
-            }
-            Console.WriteLine("Testing success rate: " + success / 10000.00 * 100.00);
-            ifsImages.Close();
-            brImages.Close();
-            ifsLabels.Close();
-            brLabels.Close();
-
-            #endregion
+          byte b = brImages.ReadByte();
+          pixels[i] = b; //считываем байт изображения в массив 
         }
-        catch (Exception ex)
+        if (nn.TrainLayer(pixels, lbl))
         {
-            Console.WriteLine(ex);
+          success++;
         }
+        Console.WriteLine("Total: " + di);
+        Console.WriteLine("Current Success Rate: " + success / (di + 1.00) * 100.00);
+        //Console.ReadLine();
+      }
+      nn.SaveLayer("layer.dat");
+      Console.WriteLine("training success rate: " + success / 60000.00 * 100.00);
+      ifsImages.Close();
+      brImages.Close();
+      ifsLabels.Close();
+      brLabels.Close();
+
+      #endregion
+
+      #region Testing
+
+      ifsLabels = new FileStream("t10k-labels.idx1-ubyte", FileMode.Open);//поток для чтения лейблов 
+      ifsImages = new FileStream("t10k-images.idx3-ubyte", FileMode.Open); // test images 
+      brLabels = new BinaryReader(ifsLabels);
+      brImages = new BinaryReader(ifsImages);
+
+      magic1 = brImages.ReadInt32(); // магическое число 
+      numImages = brImages.ReadInt32(); //количество изображений 
+      numRows = brImages.ReadInt32(); //количество строк в изображении 
+      numCols = brImages.ReadInt32(); //количество столбцов изображения 
+
+      magic2 = brLabels.ReadInt32(); //магическое число 
+      numLabels = brLabels.ReadInt32(); //количество лейблов
+
+      success = 0;
+      nn = nn.LoadLayer("layer.dat");
+      for (int di = 0; di < 10000; ++di)
+      {
+        byte lbl = brLabels.ReadByte(); //текущее значение лейбла 
+        for (int i = 0; i < 28 * 28; ++i)
+        {
+          byte b = brImages.ReadByte();
+          pixels[i] = b; //считываем байт изображения в массив 
+        }
+        if (nn.TestLayer(pixels, lbl))
+        {
+          success++;
+        }
+      }
+      Console.WriteLine("Testing success rate: " + success / 10000.00 * 100.00);
+      ifsImages.Close();
+      brImages.Close();
+      ifsLabels.Close();
+      brLabels.Close();
+
+      #endregion
     }
+    catch (Exception ex)
+    {
+      Console.WriteLine(ex);
+    }
+  }
 }
